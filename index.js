62b516d624c245c49ae10b33b00d191235c6b539
James Glenn <jglenn1@gmail.com>
2015-11-17 14:13:02 -0600

    here we go

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var PORT = 8080;
 
 var app = express();
 app.get('/', function(req, res) {
-    res.send('Hello, Docker Hub CI');
+    res.send('Hello Docker Hub CI');
 });
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
+
+module.exports = app;
